5b2d396a20d45813339a802f11d8b6d165d20079
Aravind-a12 <102940905+Aravind-a12@users.noreply.github.com>
2022-09-16 12:56:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,16 +9,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/hello', (req, res) => {
   res.send({ express: 'Hello From Heroku' });
 });
-
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'demo/build')));
-      
-    // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'demo/build', 'index.html'));
-    });
-  }
 
 app.listen(process.env.PORT || 5000, (req, res) => {
         console.log("Server is running at port 5000")
