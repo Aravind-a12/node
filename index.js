8b874d50160c4a95b268acac582ac7efe4f3b50a
Aravind-a12 <102940905+Aravind-a12@users.noreply.github.com>
2022-09-18 20:15:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,15 +10,6 @@ app.get('/', (req, res) => {
   res.send({ express: 'Hello From Heroku' });
 });
 
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'demo/build')));
-      
-    // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'demo/build', 'index.html'));
-    });
-  }
 
 app.listen(process.env.PORT || 5000, (req, res) => {
         console.log("Server is running at port 5000")
